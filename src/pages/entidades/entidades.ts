d52f16e5311853c8a58fe3358783320eaf15fed5
Gio Valdes <gio@sietecerouno.com>
2018-05-17 20:23:36 -0500

    entidades

--- src/pages/entidades/entidades.ts
+++ src/pages/entidades/entidades.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import {ServiceBankProvider} from '../../providers/service-bank/service-bank';
+import { Http } from '@angular/http';
+ 
 /**
  * Generated class for the EntidadesPage page.
  *
@@ -16,22 +18,118 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class EntidadesPage 
 {
 
- public autocompleteTags = [];
-  public autocompleteItems = [
-    'BANCO DE LA REPÚBLICA',
-    'BANCO DE BOGOTÁ',
-    'BANCO POPULAR',
-    'BANCO CORPBANCA COLOMBIA S.A.',
-    'BANCOLOMBIA',
-    'CITIBANK COLOMBIA',
-    'BANCO GNB SUDAMERIS COLOMBIA',
-    'BBVA COLOMBIA'
-  ];
+    check = true;
+    banks = [];
+    name = [];
+    public autocompleteTags = [];
+    public autocompleteItems = [
+        'BANCO DE LA REPÚBLICA',
+        'BANCO DE BOGOTÁ',
+        'BANCO POPULAR',
+        'BANCO CORPBANCA COLOMBIA S.A.',
+        'BANCOLOMBIA',
+        'CITIBANK COLOMBIA',
+        'BANCO GNB SUDAMERIS COLOMBIA',
+        'BBVA COLOMBIA'
+      ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+
+
+    country: any;
+    countries: any[];
+    filteredCountriesSingle: any[];
+
+    
+  constructor(public navCtrl: NavController, public navParams: NavParams, private serviceBankProvider: ServiceBankProvider, public http: Http) 
+  {
+    let url = "https://www.datos.gov.co/resource/sr9n-792w.json";
+
+    this.http.get(url).map(res => res.json()).subscribe(data => {
+
+        console.log(data);
+
+    });
   }
+  
+  filterCountrySingle(event) {
+        let query = event.query;
+        this.serviceBankProvider.getCountries().then(countries => {
+            this.filteredCountriesSingle = this.filterCountry(query, countries);
+            
+        });
+    }
+    
+    filterCountry(query, countries: any[]):any[] {
+        
+        let filtered : any[] = [];
+        for(let i = 0; i < countries.length; i++) {
+            let country = countries[i];
+            
+            if(country.name.toLowerCase().indexOf(query.toLowerCase()) == 0) {
+            
+                filtered.push(country);
+            }
+        }
+        return filtered;
+    }
+  
+  onInputTime (data)
+  {
+    console.log("onInputTime", data)
+  }
+  
+  onTagAdded(data)
+  {
+    console.log(data)
+  }
+  
+  
+  onSelectedTag (tagName) {
+    console.log("select")
+            
+    }
+          
+      onTagRemoved (tagName: string)
+      {
+        console.log("remove")
+        
+      }
+      
+      
+  showVal(data)
+  {
+    data.target.style.display = "none"
+    this.check = false
+    console.log("showVal", data.target.style.display)
+    
+    setTimeout(function () 
+    {
+        console.log("showVal", data.target.style.display);
+        data.target.style.display = "none"
+        console.log("onInputTime", data)
+    }, 1000);
+  }
+  
+  inputChanged(event) 
+  {
+  
+    let key = event.keyCode;
+    
+    switch(key) {
+      
+      case 13: //Enter
+        this.autocompleteTags.push({name: this.name, banks: this.banks});
+        event.preventDefault();
+        break;
+        }
+     
+  }
+  
+  
 
   ionViewDidLoad() {
+  
     console.log('ionViewDidLoad EntidadesPage');
   }
 
