929952fa7c17513c62cccba70c5b076e8d0faaf1
gabriel duncan <mamabato@gmail.com>
2018-05-23 05:08:07 -0500

    assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -73,7 +73,7 @@ interface System {
 
 export class WebpackTranslateLoader implements TranslateLoader {
   getTranslation(lang: string): Observable<any> {
-    return Observable.fromPromise(System.import(`../../assets/i18n/${lang}.json`));
+    return Observable.fromPromise(System.import(`../assets/i18n/${lang}.json`));
   }
 }
 
