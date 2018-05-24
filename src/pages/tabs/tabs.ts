fc634d43233cfe16261b8c4a47d22bdfa21efe31
gabriel duncan <mamabato@gmail.com>
2018-05-24 05:38:48 -0500

    limpieza

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -26,14 +26,9 @@ export class TabsPage {
     public events: Events,
     public screenshot: Screenshot
   ) {
-    //this.tabBarElement = document.querySelector('#tabs .tabbar');
-    //this.tabBarElementBtn = document.querySelector('#tabs .tabbar .tab-button');
+    
     this.events.subscribe('tabs:hide', () => {
-      //this.showTabs = false; background: #0669eb !important;
-      //this.tabBarElement.style.background = "#0669eb !important";
-      //this.tabBarElementBtn.style.opacity = "0";
-      //var tabBarElement = document.querySelector('#tabs .tabbar');
-      //var tabBarElementBtn = document.querySelector('#tabs .tabbar .tab-button');
+
       var tabBarElement = document.getElementsByClassName('tabbar') as HTMLCollectionOf<HTMLElement>;
       //var tabBarElementBtn = document.getElementsByClassName('tab-button') as HTMLCollectionOf<HTMLElement>;
       if (tabBarElement.length != 0) {
@@ -56,24 +51,6 @@ export class TabsPage {
                 );
 
             }, 1000);
-              /*for(let j = 0; j < tabBarElementBtn.length; j++ ){
-                tabBarElementBtn[j].style.opacity = "0";
-                // screenshot
-                if(j == tabBarElementBtn.length - 1){
-                  this.screenshot.save('jpg', 80, 'myscreenshot_trm').then(
-                    (res)=>{
-                      console.log(res);
-                      this.events.publish('tabs:unhide', res.filePath);
-                    },
-                    (err)=>{
-                      console.log(err);
-                    }
-                  );
-                }
-
-              }*/
-
-            //}
 
           }
 
@@ -83,24 +60,5 @@ export class TabsPage {
 
     });
 
-    /*this.events.subscribe('tabs:unhide', (picture) => {
-      //this.showTabs = true;
-      //document.querySelector('#tabs .tabbar').style.background = "#f8f8f8 !important";
-      //document.querySelector('#tabs .tabbar .tab-button').style.opacity = "1";
-      var tabBarElement = document.getElementsByClassName('tabbar') as HTMLCollectionOf<HTMLElement>;
-      var tabBarElementBtn = document.getElementsByClassName('tab-button') as HTMLCollectionOf<HTMLElement>;
-      var auximg = document.getElementById('imgCanvas') as HTMLImageElement;
-      auximg.style.display = 'none';
-
-      if (tabBarElement.length != 0) {
-
-        tabBarElement[0].style.background = "#f8f8f8 !important";
-      }
-      if (tabBarElementBtn.length != 0) {
-        for(let i = 0; i < tabBarElementBtn.length; i++ ){
-          tabBarElementBtn[i].style.opacity = "1";
-        }
-      }
-    });*/
   }
 }
